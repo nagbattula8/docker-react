4f0c6fa96b7e538ab3c2cb33cac94c047570bae0
nagbattula8 <nagbattula8@gmail.com>
2021-09-13 13:10:46 -0500

    changed app text

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/hi/i);
+  const linkElement = screen.getByText(/Feature/i);
   expect(linkElement).toBeInTheDocument();
 });
 
